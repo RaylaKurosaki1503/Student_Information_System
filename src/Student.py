332519742e2d70789c18eb984ff07af2bec576c6
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-02-17 20:43:04 -0500

    Update Student.py

--- src/Student.py
+++ src/Student.py
@@ -1,14 +1,23 @@
+################################################################################
 """
 Author: Rayla Kurosaki
 
-File: Student.py
+File: student.py
 
 Description:
 """
+################################################################################
 
 
 class Student:
+    """
+
+    """
     def __init__(self, name):
+        """
+
+        :param name:
+        """
         self.name = name
         self.courses = []
         self.gpa = 0.0
@@ -16,26 +25,58 @@ class Student:
         pass
 
     def get_name(self):
+        """
+
+        :return:
+        """
         return self.name
 
     def get_courses(self):
+        """
+
+        :return:
+        """
         return self.courses
 
     def add_course(self, course):
+        """
+
+        :param course:
+        :return:
+        """
         self.courses.append(course)
         pass
 
     def get_gpa(self):
+        """
+
+        :return:
+        """
         return self.gpa
 
     def set_gpa(self, gpa):
+        """
+
+        :param gpa:
+        :return:
+        """
         self.gpa = gpa
         pass
 
     def get_gpa_history(self):
+        """
+
+        :return:
+        """
         return self.gpa_history
 
     def add_to_gpa_history(self, term, gpa):
+        """
+
+        :param term:
+        :param gpa:
+        :return:
+        """
         self.gpa_history[term] = gpa
         pass
 
