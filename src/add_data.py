11c070ef0643d1edc34d6b17680cdda91a48bae7
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-02-18 01:00:54 -0500

    Update add_data.py

--- src/add_data.py
+++ src/add_data.py
@@ -14,6 +14,21 @@ from course import Course
 import rayla.excel
 
 
+def is_correct_course(term, course_id, name, course):
+    """
+
+    :param term:
+    :param course_id:
+    :param name:
+    :param course:
+    :return:
+    """
+    c1 = term == course.get_term()
+    c2 = course_id == course.get_id()
+    c3 = name == course.get_name()
+    return c1 and c2 and c3
+
+
 def add_courses(student, workbook):
     """
 
@@ -62,25 +77,22 @@ def add_assignments(student, workbook):
             term, id, sxn, name, type, weight = row
             # Iterate through the courses the student has taken
             for course in student.get_courses():
-                c1 = term == course.get_term()
-                c2 = id + "." + sxn == course.get_id()
-                c3 = name == course.get_name()
                 # If this assignment belongs to this course
-                if c1 and c2 and c3:
+                if is_correct_course(term, id + "." + sxn, name, course):
                     # Create a new assignment type and add it to this course.
                     assignment = Assignment(type, float(weight))
-                    Course.add_assignment(course, type, assignment)
-                    # Special case where
+                    course.add_assignment(type, assignment)
+                    # Course.add_assignment(course, type, assignment)
+                    # Special case
                     c4 = id + "." + sxn == "PHYS-320.01"
                     c5 = id + "." + sxn == "PHYS-321.01"
                     c6 = type == "Homework and Quiz"
                     if (c4 or c5) and c6:
                         assignment = Assignment("Homework", float(weight))
-                        Course.add_assignment(course, "Homework", assignment)
+                        course.add_assignment("Homework", assignment)
                         assignment = Assignment("Quiz", float(weight))
-                        Course.add_assignment(course, "Quiz", assignment)
+                        course.add_assignment("Quiz", assignment)
                         pass
-                    # To save time, break out of this loop.
                     break
                     pass
                 pass
@@ -108,17 +120,17 @@ def add_grades(student, workbook):
         if not (i == 0):
             # Unpack the row
             term, id, sxn, name, type, raw, curved, notes = row
-            # Iterate through the courses the student has taken
-            for course in student.get_courses():
-                c1 = term == course.get_term()
-                c2 = id + "." + sxn == course.get_id()
-                c3 = name == course.get_name()
-                # If this assignment belongs to this course
-                if c1 and c2 and c3:
-                    # Add teh grade to the correct assignment type
-                    assignment = Course.get_assignments(course)[type]
-                    Assignment.add_grade(assignment, raw)
-                    break
+            # Exit the loop if there is no grade for the assignment
+            if not ((raw is None) and (curved is None)):
+                # Iterate through the courses the student has taken
+                for course in student.get_courses():
+                    # If this assignment belongs to this course
+                    if is_correct_course(term, id + "." + sxn, name, course):
+                        # Add the grade to the correct assignment type
+                        assignment = course.get_assignments()[type]
+                        assignment.add_grade(raw)
+                        break
+                        pass
                     pass
                 pass
             pass
@@ -135,6 +147,29 @@ def add_extra_credit(student, workbook):
     :param workbook:
     :return:
     """
+    # Get the worksheet that contains all the courses the student has taken
+    ws = rayla.excel.get_worksheet(workbook, "extra_credit")
+    # Set an index counter
+    i = 0
+    # Iterate through each row of the worksheet
+    for row in ws.values:
+        # If the row is not the first row
+        if not (i == 0):
+            # Unpack the row
+            term, id, sxn, name, extra_credit = row
+            # Iterate through the courses the student has taken
+            for course in student.get_courses():
+                # If this assignment belongs to this course
+                if is_correct_course(term, id + "." + sxn, name, course):
+                    # Set the extra credit the student has earned for that class
+                    course.set_extra_credit(extra_credit)
+                    break
+                    pass
+                pass
+            pass
+        # Increment the index counter by 1
+        i += 1
+        pass
     pass
 
 
@@ -145,6 +180,93 @@ def add_grading_scale(student, workbook):
     :param workbook:
     :return:
     """
+    letters = ["A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "D-"]
+    # Get the worksheet that contains all the courses the student has taken
+    ws = rayla.excel.get_worksheet(workbook, "grading_scales")
+    # Set an index counter
+    i = 0
+    # Iterate through each row of the worksheet
+    for row in ws.values:
+        # If the row is not the first row
+        if not (i == 0):
+            # Unpack the row
+            term = row[0]
+            course_id = row[1] + "." + row[2]
+            name = row[3]
+            # Create the grading scale
+            grading_scale = {}
+            for i in range(len(letters)):
+                num = row[i + 4]
+                if not (num is None):
+                    grading_scale[letters[i]] = float(num)
+            # Iterate through the courses the student has taken
+            for course in student.get_courses():
+                # If this assignment belongs to this course
+                if is_correct_course(term, course_id, name, course):
+                    # Set the grading scale for that class
+                    course.set_grading_scale(grading_scale)
+                    break
+                    pass
+                pass
+            pass
+        # Increment the index counter by 1
+        i += 1
+        pass
+    pass
+
+
+def add_drop_count(student, workbook):
+    """
+
+    :param student:
+    :param workbook:
+    :return:
+    """
+    # Get the worksheet that contains all the types of assignments
+    ws = rayla.excel.get_worksheet(workbook, "drop_grades")
+    # Set the index counter
+    i = 0
+    # Iterate through each row of the worksheet
+    for row in ws.values:
+        # If it is not the first row
+        if not (i == 0):
+            # Unpack the row
+            term, id, sxn, name, type, count = row
+            # Iterate through the courses the student has taken
+            for course in student.get_courses():
+                # If this assignment belongs to this course
+                if is_correct_course(term, id + "." + sxn, name, course):
+                    assignments = course.get_assignments()
+                    assignment = assignments[type]
+                    assignment.set_drop_count(count)
+                    break
+                    pass
+                pass
+            pass
+        # Increment the index counter
+        i += 1
+        pass
+    pass
+
+
+def add_other(student):
+    """
+
+    :param student:
+    :return:
+    """
+    for course in student.get_courses():
+        if course.get_id() in ["PHYS-320.01", "PHYS-321.01"]:
+            assignments = course.get_assignments()
+            hw_n_quiz = []
+            for grade in assignments["Homework"].get_grades():
+                hw_n_quiz.append(grade)
+                pass
+            for grade in assignments["Quiz"].get_grades():
+                hw_n_quiz.append(grade)
+                pass
+            assignment = assignments["Homework and Quiz"]
+            assignment.set_grades(hw_n_quiz)
     pass
 
 
@@ -160,3 +282,6 @@ def main(student, workbook):
     add_grades(student, workbook)
     add_extra_credit(student, workbook)
     add_grading_scale(student, workbook)
+    add_drop_count(student, workbook)
+    add_other(student)
+    pass
