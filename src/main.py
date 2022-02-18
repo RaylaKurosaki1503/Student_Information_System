6dc0cccb46daf54ccb5e156c2039e51e613dd598
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-02-17 20:43:02 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+################################################################################
 """
 Author: Rayla Kurosaki
 
@@ -5,33 +6,27 @@ File: main.py
 
 Description:
 """
-import Assignment
-import Course
-from Student import Student
+################################################################################
+
+
+from student import Student
 import add_data
 import rayla.excel
 
 
 def main():
-    # Creating/Initializing a student given their name, major(s), and minor(s)
-    name = "Rayla Kurosaki"
-    majors = ["Computational Mathematics",
-              "Applied and Computational Mathematics"]
-    minors = ["Physics"]
+    """
 
+    :return:
     """
-    """
+    # Creating/Initializing a student
+    name = "Rayla Kurosaki"
     stu = Student(name)
+    # Ask the user to input the absolute path to the Excel workbook
     path = input("Enter the absolute path to the excel workbook: ")
     wb = rayla.excel.get_workbook(path)
 
-    add_data.add_data_main(stu, wb)
-
-    for course in stu.get_courses():
-        Course.pp_print(course)
-        for type, assignment in Course.Course.get_assignments(course).items():
-            Assignment.pp_print(assignment)
-        print("")
+    add_data.main(stu, wb)
     return
 
 
