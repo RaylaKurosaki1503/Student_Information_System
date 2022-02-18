369e2a32cc13cf87a40194e3b43493c29bf8a6f8
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-02-17 20:43:10 -0500

    Update Course.py

--- src/Course.py
+++ src/Course.py
@@ -1,14 +1,27 @@
+################################################################################
 """
 Author: Rayla Kurosaki
 
-File: Course.py
+File: course.py
 
 Description:
 """
+################################################################################
 
 
 class Course:
+    """
+
+    """
     def __init__(self, term, id, name, credit, prof):
+        """
+
+        :param term:
+        :param id:
+        :param name:
+        :param credit:
+        :param prof:
+        """
         self.term = term
         self.id = id
         self.name = name
@@ -25,28 +38,62 @@ class Course:
         pass
 
     def get_term(self):
+        """
+
+        :return:
+        """
         return self.term
 
     def get_id(self):
+        """
+
+        :return:
+        """
         return self.id
 
     def get_name(self):
+        """
+
+        :return:
+        """
         return self.name
 
     def get_credit(self):
+        """
+
+        :return:
+        """
         return self.credit
 
     def get_prof(self):
+        """
+
+        :return:
+        """
         return self.prof
 
     def get_grading_scale(self):
+        """
+
+        :return:
+        """
         return self.grading_scale
 
     def add_to_grading_scale(self, letter, num):
+        """
+
+        :param letter:
+        :param num:
+        :return:
+        """
         self.grading_scale[letter] = num
         pass
 
     def get_assignments(self):
+        """
+
+        :return:
+        """
         return self.assignments
 
     def add_assignment(self, type, assignment):
@@ -54,50 +101,118 @@ class Course:
         pass
 
     def get_extra_credit(self):
+        """
+
+        :return:
+        """
         return self.extra_credit
 
+    def add_extra_credit(self, extra_credit):
+        """
+
+        :param extra_credit:
+        :return:
+        """
+        self.extra_credit += extra_credit
+        pass
+
     def set_extra_credit(self, extra_credit):
+        """
+
+        :param extra_credit:
+        :return:
+        """
         self.extra_credit = extra_credit
         pass
 
     def get_raw_grade(self):
+        """
+
+        :return:
+        """
         return self.raw_grade
 
     def set_raw_grade(self, grade):
+        """
+
+        :param grade:
+        :return:
+        """
         self.raw_grade = grade
         pass
 
     def get_letter_grade(self):
+        """
+
+        :return:
+        """
         return self.letter_grade
 
     def set_letter_grade(self, letter):
+        """
+
+        :param letter:
+        :return:
+        """
         self.letter_grade = letter
         pass
 
     def get_final_grade(self):
+        """
+
+        :return:
+        """
         return self.final_grade
 
     def set_final_grade(self, letter):
+        """
+
+        :param letter:
+        :return:
+        """
         self.final_grade = letter
         pass
 
     def get_gpa(self):
+        """
+
+        :return:
+        """
         return self.gpa
 
     def set_gpa(self, gpa):
+        """
+
+        :param gpa:
+        :return:
+        """
         self.gpa = gpa
         pass
 
     def get_points(self):
+        """
+
+        :return:
+        """
         return self.points
 
     def set_points(self, points):
+        """
+
+        :param points:
+        :return:
+        """
         self.points = points
 
     pass
 
 
 def pp_print(course):
+    """
+
+    :param course:
+    :return:
+    """
     print([course.get_term(), course.get_id(), course.get_name(),
            course.get_credit(), course.get_assignments()])
     pass
