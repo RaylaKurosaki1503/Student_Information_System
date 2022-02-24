e8e5ecd1708437d614c869afc103b63c6f9e1b74
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-02-24 09:25:21 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,8 +7,6 @@ File: main.py
 Description:
 """
 ###############################################################################
-
-
 import sys
 import rayla.excel
 from algorithm import phase1_add_data
@@ -26,8 +24,7 @@ def main():
     name = "Rayla Kurosaki"
     stu = Student(name)
     # Ask the user to input the absolute path to the Excel workbook
-    # path = input("Enter the absolute path to the excel workbook: ")
-    path = sys.argv[1]
+    path = input("Enter the absolute path to the excel workbook: ")
     # Get the Excel Spreadsheet
     wb = rayla.excel.get_workbook(path)
     # Add data to the student's database
@@ -36,14 +33,6 @@ def main():
     phase2_modify_data.main(stu, wb)
     # Print the student's transcript
     phase3_print_transcript.main(stu)
-
-    # for k, v in stu.get_gpa_history().items():
-    #     #     print(k, v)
-    #     #
-    #     # for course in stu.get_courses():
-    #     #     print([course.get_term(), course.get_id(), course.get_name(),
-    #     #            course.get_raw_grade(), course.get_letter_grade(),
-    #     #            course.get_final_grade()])
     return
 
 
