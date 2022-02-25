7c40863fa442d6b1877e02b44eef455926d0729d
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-02-25 00:46:57 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
-###############################################################################
 """
 Author: Rayla Kurosaki
 
 File: main.py
 
-Description:
+Description: This file runs the entire program.
 """
-###############################################################################
 import sys
+
 import rayla.excel
 from algorithm import phase1_add_data
 from algorithm import phase2_modify_data
@@ -17,24 +16,35 @@ from constructors.student import Student
 
 def main():
     """
-
-    :return:
+    This function will ask the user to input the following
+        1. Their name
+        2. The absolute path to the Microsoft Excel Workbook/Spreadsheet
+    Once the user finished the tasks above, it will create a student from the
+    name, and it will get access to the Microsoft Excel Workbook/Spreadsheet.
+    After creating a student and getting access to the Microsoft Excel
+    Workbook/Spreadsheet, it will call one function to add all the relevant
+    data into the student, one function to perform all the calculations, and one
+    function to print the results in the form of a Transcript.
     """
     # Creating/Initializing a student
+    # name = input("Enter your name: ")
     name = "Rayla Kurosaki"
-    stu = Student(name)
+    student = Student(name)
     # Ask the user to input the absolute path to the Excel workbook
-    path = input("Enter the absolute path to the excel workbook: ")
+    # path = input("Enter the absolute path to the excel workbook: ")
+    path = sys.argv[1]
     # Get the Excel Spreadsheet
-    wb = rayla.excel.get_workbook(path)
+    workbook = rayla.excel.get_workbook(path)
     # Add data to the student's database
-    phase1_add_data.main(stu, wb)
-    # Manipulate the student database
-    phase2_modify_data.main(stu, wb)
+    phase1_add_data.main(student, workbook)
+    # Manipulate the student's database
+    phase2_modify_data.main(student, workbook)
     # Print the student's transcript
-    phase3_print_transcript.main(stu)
+    phase3_print_transcript.main(student)
     return
 
 
 if __name__ == '__main__':
     main()
+    # print(sys.argv)
+    pass
