2b5e0b8d0538c4cd164f94ef6718eda87017e301
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-02-25 00:46:59 -0500

    Update student.py

--- src/constructors/student.py
+++ src/constructors/student.py
@@ -1,20 +1,19 @@
-################################################################################
 """
 Author: Rayla Kurosaki
 
 File: student.py
 
-Description:
+Description: This file contains all the functionality of a student.
 """
-################################################################################
 
 
 class Student:
-    """
-
-    """
-
     def __init__(self, name):
+        """
+        Creates an instance of a student.
+
+        :param name: Name of the student.
+        """
         self.name = name
         self.courses = []
         self.gpa = 0.0
@@ -23,58 +22,70 @@ class Student:
 
     def get_name(self):
         """
+        Gets the name of the student.
 
-        :return:
+        :return: The name of the student.
         """
         return self.name
 
     def get_courses(self):
         """
+        Gets the list of courses the student has taken.
 
-        :return:
+        :return: A list of courses the student has taken
         """
         return self.courses
 
     def add_course(self, course):
         """
+        Adds a new course to the list of courses the student has taken.
 
-        :param course:
-        :return:
+        :param course: A new course the student has taken
         """
         self.courses.append(course)
         pass
 
     def get_gpa(self):
         """
+        Gets the student's current GPA.
 
-        :return:
+        :return: The student's current GPA.
         """
         return self.gpa
 
     def set_gpa(self, gpa):
         """
+        Sets the student's current GPA.
 
-        :param gpa:
-        :return:
+        :param gpa: The student's current GPA.
         """
         self.gpa = gpa
         pass
 
     def get_gpa_history(self):
         """
+        Get a dictionary which contains the student's GPA for each term/semester.
 
-        :return:
+        :return: A dictionary which contains the student's GPA for each
+                 term/semester.
         """
         return self.gpa_history
 
     def add_to_gpa_history(self, term, gpa):
         """
+        Adds a term-GPA pair to the student's database.
 
-        :param term:
-        :param gpa:
-        :return:
+        :param term: The term the student earn their GPA in.
+        :param gpa: The GPA the student earned in a specific semester.
         """
         self.gpa_history[term] = gpa
+        pass
+
+    def print_student(self):
+        """
+        Pretty prints the student's basic info.
+        """
+        print(f"Name: {self.name}")
         pass
 
     pass
