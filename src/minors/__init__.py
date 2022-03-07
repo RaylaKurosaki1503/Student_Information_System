e55aafa8653df56b0047215f1b897edb8eefc45e
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-03-07 15:54:21 -0500

    Update __init__.py

---
+++ src/minors/__init__.py
@@ -0,0 +1,11 @@
+"""
+Author: Rayla Kurosaki
+
+File: __init__.py
+
+Description: The initialization file for the minors library/package.
+"""
+
+from minors.minor import *
+from minors.phys import *
+from minors.compsci import *
