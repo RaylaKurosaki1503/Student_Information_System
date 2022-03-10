1a1933610f4042ee2c79972483ac1efdef5bf3ef
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-03-10 17:26:28 -0500

    Create __init__.py

---
+++ src/degrees/__init__.py
@@ -0,0 +1,10 @@
+"""
+Author: Rayla Kurosaki
+
+File: __init__.py
+
+Description: The initialization file for the package degrees.
+"""
+
+from degrees.cmth_2017 import *
+from degrees.mathematics import *
