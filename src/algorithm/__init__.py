34c68a87e9a96c227003d9e375eca8e64ea87bbf
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-03-09 23:47:44 -0500

    Update __init__.py

--- src/algorithm/__init__.py
+++ src/algorithm/__init__.py
@@ -3,12 +3,10 @@ Author: Rayla Kurosaki
 
 File: __init__.py
 
-Description: The initialization file for the algorithm library/package.
+Description: The initialization file for the package algorithm.
 """
 
-from algorithm.helper_functions import *
 from algorithm.phase0_create_student import *
 from algorithm.phase1_add_data import *
 from algorithm.phase2_modify_data import *
 from algorithm.phase3_print_transcript import *
-from algorithm.special_calculations import *
