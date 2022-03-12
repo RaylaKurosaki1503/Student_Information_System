2f46a95c0234f114c2aa6568ad9d8d9a22c120fa
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-03-12 00:21:25 -0500

    Update excel.py

--- src/__utils__/excel.py
+++ src/__utils__/excel.py
@@ -28,183 +28,239 @@ def get_workbook(path):
     return openpyxl.load_workbook(path)
 
 
-def save_workbook(wb, filename):
+def save_workbook(workbook, filename):
     """
     Saves all changes to the existing workbook if the filename/path is the
     same as the initial filename/path or creates a new workbook if the
     filename/path is different from the initial filename/path.
 
-    :param wb: Current workbook to manipulate.
+    :param workbook: Current workbook to manipulate.
     :param filename: Name of the saved workbook (filename.xlsx), or the
                      Absolute path to where it should be saved
                      ('C:\...\filename.xlsx').
-    :return: None
     """
-    wb.save(filename)
+    workbook.save(filename)
     pass
 
 
-def create_new_worksheet(wb, name):
+def create_new_worksheet(workbook, name, pos=None):
     """
-    Creates a new worksheet and adds it to the end of the workbook.
+    Creates a new worksheet and either adds it at a specific position, or
+    adds it to the end of the workbook if no position is given.
 
-    :param wb: Current workbook to manipulate.
+    :param workbook: Current workbook to manipulate.
     :param name: Name of the new worksheet.
-    :return: None
+    :param pos: Inserting the worksheet at position pos.
     """
-    wb.create_sheet(name)
+    # if the user doesn't specify a position to insert the sheet at.
+    if pos is None:
+        # Insert the sheet at the end of the workbook.
+        workbook.create_sheet(name)
+        pass
+    # Otherwise.
+    else:
+        # Insert the sheet at the user's desired position.
+        workbook.create_sheet(name, pos)
+        pass
     pass
 
 
-def get_worksheet_names(wb):
+def get_worksheet(workbook, worksheet_name=None):
+    """
+    Returns the active worksheet or the worksheet with the specified name.
+
+    :param workbook: Current workbook to manipulate.
+    :param worksheet_name: Name of the worksheet to get.
+    :return: The active worksheet of this workbook or the worksheet with the
+             correct worksheet name.
+    """
+    # If the user doesn't give a worksheet name to search for.
+    if worksheet_name is None:
+        # Get the active worksheet.
+        return workbook.active
+    # Otherwise.
+    else:
+        # Get the worksheet with the correct worksheet name.
+        return workbook[worksheet_name]
+    pass
+
+
+def copy_worksheet(workbook, worksheet):
+    """
+    Creates a copy of the worksheet.
+
+    :param workbook: The workbook the worksheet is from.
+    :param worksheet: Worksheet to copy.
+    :return: A copy of the worksheet.
+    """
+    return workbook.copy_worksheet(worksheet)
+
+
+def get_worksheet_names(workbook):
     """
     Gets the names of all the worksheets of the current workbook.
 
-    :param wb: Current workbook to manipulate.
+    :param workbook: Current workbook to manipulate.
     :return: A list of all the worksheets' names of the current workbook.
     """
-    return wb.sheetnames
+    return workbook.sheetnames
 
 
-def get_active_worksheet(wb):
-    """
-    Gets the active worksheet of the current workbook. The active worksheet
-    is the sheet that pops up when you open the workbook.
-
-    :param wb: Current workbook to manipulate.
-    :return: The active worksheet of this workbook.
-    """
-    return wb.active
-
-
-def get_worksheet(wb, ws_name):
-    """
-    Returns the worksheet with a specific name.
-
-    :param wb: Current workbook to manipulate.
-    :param ws_name: Name of the worksheet to get.
-    :return: The worksheet with the correct worksheet name
-    """
-    return wb[ws_name]
-
-
-def update_worksheet_name(ws, name):
+def update_worksheet_name(worksheet, name):
     """
     Update/Change the name of a specific worksheet.
 
-    :param ws: Current worksheet to manipulate.
+    :param worksheet: Current worksheet to manipulate.
     :param name: New name of the worksheet
     :return: None
     """
-    ws.title = name
+    worksheet.title = name
     pass
 
 
-def get_cell_value(ws, cell):
+def get_cell_value(worksheet, cell):
     """
     Gets the value of a specific cell of the current worksheet.
 
-    :param ws: Current worksheet to manipulate.
+    :param worksheet: Current worksheet to manipulate.
     :param cell: Cell to get the value of.
     :return: The value of a specific cell of the current worksheet.
     """
-    return ws[cell].value
+    return worksheet[cell].value
 
 
-def update_cell_value(ws, cell, value):
+def update_cell_value(worksheet, cell, value):
     """
     Update/Change the value of a specific cell of the current worksheet.
 
-    :param ws: Current worksheet to manipulate.
+    :param worksheet: Current worksheet to manipulate.
     :param cell: Cell to change the value of.
     :param value:
-    :return: None
     """
-    ws[cell] = value
+    worksheet[cell] = value
     pass
 
 
-def add_data_to_worksheet(ws, lst):
+def merge_cells(worksheet, cell1, cell2):
+    """
+    Merges the cells from call1 to cell2 in a worksheet.
+
+    :param worksheet: Current worksheet to manipulate.
+    :param cell1: First cell in the range.
+    :param cell2: Last cell in the range.
+    """
+    worksheet.merge_cells(f"{cell1}:{cell2}")
+    pass
+
+
+def unmerge_cells(worksheet, cell1, cell2):
+    """
+    Unmerges the cells from call1 to cell2 in a worksheet.
+
+    :param worksheet: Current worksheet to manipulate.
+    :param cell1: First cell in the range.
+    :param cell2: Last cell in the range.
+    """
+    worksheet.unmerge_cells(f"{cell1}:{cell2}")
+    pass
+
+
+def add_data_to_worksheet(worksheet, lst):
     """
     Adds a row of data to the end of the worksheet
 
-    :param ws: Current worksheet to manipulate.
+    :param worksheet: Current worksheet to manipulate.
     :param lst: A list of values to add.
-    :return: None
     """
-    ws.append(lst)
+    worksheet.append(lst)
     pass
 
 
-def insert_empty_row(ws, i):
+def insert_empty_rows(worksheet, pos, n=None):
     """
-    Inserts an empty row at the i-th position of the current worksheet.
+    Inserts n empty rows at position pos if n is given. If n is not given,
+    insert one empty row at position pos instead.
 
-    :param ws: Current worksheet to manipulate.
-    :param i: Location of the worksheet to add the empty row
-              (starts at pos1).
-    :return: None
+    :param worksheet: Current worksheet to manipulate.
+    :param pos: Location of the worksheet to add the empty row (starts at
+                pos1).
+    :param n: Number of rows to insert from position pos.
     """
-    ws.insert_rows(i)
+    if n is None:
+        worksheet.insert_rows(pos)
+        pass
+    else:
+        worksheet.insert_rows(pos, n)
+        pass
     pass
 
 
-def delete_row(ws, i):
+def insert_empty_cols(worksheet, pos, n=None):
+    """
+    Inserts n empty columns at position pos if n is given. If n is not given,
+    insert one empty columns at position pos instead.
+
+    :param worksheet: Current worksheet to manipulate.
+    :param pos: Location of the worksheet to add the empty columns (starts at
+                pos1).
+    :param n: Number of columns to insert from position pos.
+    """
+    if n is None:
+        worksheet.insert_cols(pos)
+        pass
+    else:
+        worksheet.insert_cols(pos, n)
+        pass
+    pass
+
+
+def delete_row(worksheet, pos, n=None):
     """
     Deletes a row at the i-th position of the current worksheet.
 
-    :param ws: Current worksheet to manipulate.
-    :param i: Location of the worksheet to delete a row (starts at pos 1).
-    :return: None
+    :param worksheet: Current worksheet to manipulate.
+    :param pos: Location of the worksheet to delete a row (starts at pos 1).
+    :param n: Number of rows to delete.
     """
-    ws.delete_rows(i)
+    if n is None:
+        worksheet.delete_rows(pos)
+        pass
+    else:
+        worksheet.delete_rows(pos, n)
+        pass
     pass
 
 
-def get_max_rows(ws):
-    """
-    Gets the number of non-empty rows of the current worksheet.
-
-    :param ws: Current worksheet to manipulate.
-    :return: Number of non-empty rows
-    """
-    return ws.max_row
-
-
-def insert_empty_col(ws, i):
-    """
-    Inserts an empty col at the i-th position of the current worksheet.
-
-    :param ws: Current worksheet to manipulate.
-    :param i: Location of the worksheet to add the empty col
-              (starts at pos 1).
-    :return: None
-    """
-    ws.insert_cols(i)
-    pass
-
-
-def delete_col(ws, i):
+def delete_col(worksheet, i):
     """
     Deletes a column at the i-th position of the current worksheet.
 
-    :param ws: Current worksheet to manipulate.
+    :param worksheet: Current worksheet to manipulate.
     :param i: Location of the worksheet to delete a column
               (starts at pos 1).
-    :return: None
     """
-    ws.delete_cols(i)
+    worksheet.delete_cols(i)
     pass
 
 
-def get_max_cols(ws):
+def get_max_rows(worksheet):
+    """
+    Gets the number of non-empty rows of the current worksheet.
+
+    :param worksheet: Current worksheet to manipulate.
+    :return: Number of non-empty rows
+    """
+    return worksheet.max_row
+
+
+def get_max_cols(worksheet):
     """
     Gets the number of non-empty columns of the current worksheet.
 
-    :param ws: Current worksheet to manipulate.
+    :param worksheet: Current worksheet to manipulate.
     :return: Number of non-empty columns
     """
-    return ws.max_column
+    return worksheet.max_column
 
 # def template_iter(ws):
 #     """
