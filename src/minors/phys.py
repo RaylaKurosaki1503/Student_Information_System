defa9467fe40dbe10fa2129920b8fd444af8ec02
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-03-13 15:09:08 -0400

    Update phys.py

--- src/minors/phys.py
+++ src/minors/phys.py
@@ -6,6 +6,7 @@ File: phys.py
 Description: This file contains the functionality to represent the Physics
              minor. The requirements have been updated on 2022/03/09.
 """
+
 from minors.minor import Minor
 
 dict_prereq = {
