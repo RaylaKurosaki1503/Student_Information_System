84c3cc0ec6d70e47ba14ba86a5e41970e69b72d1
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-03-14 22:36:21 -0400

    Update minor.py

--- src/minors/minor.py
+++ src/minors/minor.py
@@ -65,18 +65,12 @@ class Minor:
         """
         Removes all electives the student did not take to fulfill the minor.
         """
-        # Initialize a temporary dictionary.
         temp_dict = {}
-        # For each elective in the minor.
         for id, course in self.electives.items():
-            # If the student took the course.
             if course is not None:
-                # Add it to the temporary dictionary.
                 temp_dict[id] = course
                 pass
             pass
-        # Clear the electives' dictionary and then update with only the
-        # electives the student took.
         self.electives.clear()
         self.electives.update(temp_dict)
         pass
@@ -85,23 +79,15 @@ class Minor:
         """
         Computes the GPA for this minor.
         """
-        # Initialize the number of earned points and earned credits.
         total_earned_credits, total_points = 0.0, 0.0
-        # For each required course.
         for course in self.req.values():
-            # Accumulate the credits earned for this course.
             total_earned_credits += course.get_earned_credit()
-            # Accumulate the points earned for this course.
             total_points += course.get_points()
             pass
-        # For each elective.
         for course in self.electives.values():
-            # Accumulate the credits earned for this course.
             total_earned_credits += course.get_earned_credit()
-            # Accumulate the points earned for this course.
             total_points += course.get_points()
             pass
-        # Compute the GPA for this minor.
         self.gpa = utils.format_num_3(total_points / total_earned_credits)
         pass
 
