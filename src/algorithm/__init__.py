1adcfde5a416c4745ebaa8b34cf39cb64f410ae3
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-03-19 01:10:38 -0400

    Update __init__.py

--- src/algorithm/__init__.py
+++ src/algorithm/__init__.py
@@ -10,3 +10,4 @@ from algorithm.phase0_create_student import *
 from algorithm.phase1_add_data import *
 from algorithm.phase2_modify_data import *
 from algorithm.phase3_print_transcript import *
+from algorithm.phase4_print_to_workbook import *
