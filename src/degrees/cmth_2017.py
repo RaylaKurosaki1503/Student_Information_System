b517c5adc9d3bd479e5927d79dab0bade1ee8ca0
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-03-19 01:10:50 -0400

    Update cmth_2017.py

--- src/degrees/cmth_2017.py
+++ src/degrees/cmth_2017.py
@@ -6,6 +6,14 @@ File: cmth_2017.py
 Description:
 """
 
+from degrees.major import Major
 
-class Cmth2017:
+
+class Cmth2017(Major):
+    def __init__(self):
+        """
+        Create an instance of a Physics minor.
+        """
+        super().__init__("Computational Mathematics", "BS")
+        pass
     pass
