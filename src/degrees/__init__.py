d23e72fffa21b75184012c1a1c21b2b73ca2f2e8
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-03-19 01:58:58 -0400

    Update __init__.py

--- src/degrees/__init__.py
+++ src/degrees/__init__.py
@@ -8,5 +8,6 @@ Description: The initialization file for the package degrees.
 
 from degrees.major import *
 
+from degrees.cmacm import *
 from degrees.cmth_2017 import *
 from degrees.mathematics import *
