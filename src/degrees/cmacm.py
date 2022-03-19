bef5c50e334923b6892e040f9930bd07ecea3775
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-03-19 01:58:59 -0400

    Create cmacm.py

---
+++ src/degrees/cmacm.py
@@ -0,0 +1,19 @@
+"""
+Author: Rayla Kurosaki
+
+File: cmacm.py
+
+Description:
+"""
+
+from degrees.major import Major
+
+
+class Cmacm(Major):
+    def __init__(self):
+        """
+        Create an instance of a Physics minor.
+        """
+        super().__init__("Applied and Computational Mathematics", "MS")
+        pass
+    pass
