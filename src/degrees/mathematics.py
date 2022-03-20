a81fe1e23f8ad77316463f6348470d4777219f3a
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-03-20 01:51:34 -0400

    Update mathematics.py

--- src/degrees/mathematics.py
+++ src/degrees/mathematics.py
@@ -3,7 +3,9 @@ Author: Rayla Kurosaki
 
 File: mathematics.py
 
-Description:
+Description: This file contains the functionality of a mathematics major.
+             This major is a custom major made to supplement the example
+             Microsoft Excel Workbook input file.
 """
 
 
