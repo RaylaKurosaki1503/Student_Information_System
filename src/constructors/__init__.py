5abab0f2a0fee386f96e86ae4af6e70307affe92
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-03-20 01:51:26 -0400

    Update __init__.py

--- src/constructors/__init__.py
+++ src/constructors/__init__.py
@@ -3,7 +3,7 @@ Author: Rayla Kurosaki
 
 File: __init__.py
 
-Description: The initialization file for the package constructors.
+Description: The initialization file for the package this file is in.
 """
 
 from constructors.assignment import *
